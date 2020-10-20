65c7b2ab507400549e9e3dd001d51feae4737977
Damon-L <397885695@qq.com>
2020-10-20 18:57:59 +0800

    master

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="tab-control">
+      <div v-for="item in titles " :key="item.id">
+          <span>{{item}}</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabControl',
+  props: {
+    titles:{
+      type:Array,
+      default(){
+          return []
+      }
+    }
+  },
+  data() {
+    return {
+    };
+  },
+  computed:{
+  },
+  watch:{
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+
+</style>
